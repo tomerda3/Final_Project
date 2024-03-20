5b239b1b0829f74da39ab0f545503194503f7aa2
maayanrab <maayara@ac.sce.ac.il>
2024-03-20 16:52:08 +0200

    adding dataset

--- src/main.py
+++ src/main.py
@@ -4,20 +4,22 @@ from models.model_names import *
 
 if __name__ == "__main__":
 
+    # Construct HHD engine
     # HHD_engine = construct_HHD_engine(
     #     base_dir=Path.cwd() / "data" / "HHD",
     #     image_shape=(400, 400, 1)
     # )
-    #
-    # # Setting engine model
+
+    # Setting engine model
     # HHD_engine.choose_model(XCEPTION)
-    #
-    # # Training model
+
+    # Training model
     # HHD_engine.train_model()
-    #
-    # # Test model
+
+    # Test model
     # HHD_engine.test_model()
 
+    # Construct KHATT engine
     KHATT_engine = construct_KHATT_engine(
         base_dir=Path.cwd() / "data" / "KHATT",
         image_shape=(400, 400, 1)
